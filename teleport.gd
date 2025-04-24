fcd944f8dc8091a1d33aa8d833c32f6b8cde33d1
Appelant <gonnot.aurelien@gmail.com>
2025-04-24 16:23:22 +0200

    Ajout de tp précise en fonction de la position xyz + tresor hitbox

--- teleport.gd
+++ teleport.gd
@@ -5,8 +5,11 @@ extends Area3D
 # Connecte le signal dans la fonction _ready()
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
+	
+	# Exemple de changement de position de téléportation
+	teleport_position = Vector3(10, 1000, -3)  # Nouvelle position de téléportation
 
-# Lorsque le joueur entre dans la zone, on le téléporte
+# Lorsque le joueur entre dans la zone, on l e téléporte
 func _on_body_entered(body: Node3D) -> void:
 	if body.name == "Player":  # Vérifie si c'est le joueur
 		body.global_transform.origin = teleport_position  # Téléporte le joueur à la position définie
