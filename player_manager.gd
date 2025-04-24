704cbd337fc13178b355970fab9f5c6182ae70c8
Zakaria <zakaria.mezouari@lecole-ldlc.com>
2025-04-24 20:03:48 +0800

    rappeler l'araignée

--- player_manager.gd
+++ player_manager.gd
@@ -2,32 +2,55 @@ extends Node3D
 
 
 var isPlayer = true
+var is_spider_attached = false
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Player.canBeControlled = true
 	$spider2.canBeControlled = false
+	is_spider_attached = true
+	update_spider_position()
+
+
+# Fonction pour garder l'araignée immobile
+func stop_spider():
+	$spider2.velocity = Vector3.ZERO
 	
 	
 
 func update_spider_position():
 	var player_pos = $Player.global_position
-	var offset = Vector3(0.5, 0, 1.2)  # Un peu à droite (x), et derrière (z)
+	var player_forward = $Player.global_transform.basis.z.normalized()  # Direction avant du joueur
+	var player_right = $Player.global_transform.basis.x.normalized()  # Direction droite du joueur
+
+	var offset = player_right * 2.0 + player_forward * -0.7  # Position décalée vers la droite et devant
 	$spider2.global_position = player_pos + offset
 	$spider2.rotation = Vector3(0, 0, 0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if isPlayer:
+	if isPlayer and is_spider_attached:
 		update_spider_position()
-	
+
 	if Input.is_action_just_pressed("switch_control"):
 		isPlayer = !isPlayer
 		if isPlayer:
 			set_player_form()
+			stop_spider()
+			is_spider_attached = false
 		else : 
 			set_spider_form()
+			is_spider_attached = true
+
+	if Input.is_action_just_pressed("recall_spider"):
+		if isPlayer:
+			is_spider_attached = true
+
+			
+	
 
 func set_player_form():
 	$spider2.visible = true
