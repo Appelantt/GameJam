b4a96fd80e2af5da83a1d5884b215ceb4daadb75
Appelant <gonnot.aurelien@gmail.com>
2025-04-25 13:37:35 +0200

    Scène fin qui marche

--- score.gd
+++ score.gd
@@ -1,7 +1,11 @@
 extends Label
 
 var score = 0
+const TOTAL_TREASURES = 8
 
 func tresor_keep():
 	score += 1
-	text = "Tresor trouver: %s" % score
+	text = "Trésor trouvé : %s" % score
+	
+	if score >= TOTAL_TREASURES:
+		get_tree().change_scene_to_file("res://cinematique.tscn")  # Mets le bon chemin ici
