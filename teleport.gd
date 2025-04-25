4e42f30f902b1976b0699b97ae66ea0766d45ff7
Appelant <gonnot.aurelien@gmail.com>
2025-04-25 10:47:22 +0200

    tp de colin

--- teleport.gd
+++ teleport.gd
@@ -1,16 +1,13 @@
 extends Area3D
 
-@export var teleport_position: Vector3  # 📍 Position où envoyer le joueur
+@export var teleport_position: Node3D  # 📍 Position où envoyer le joueur
 
 # Connecte le signal dans la fonction _ready()
 func _ready():
 	connect("body_entered", Callable(self, "_on_body_entered"))
-	
-	# Exemple de changement de position de téléportation
-	teleport_position = Vector3(25, 1.201, -860.795 )  # Nouvelle position de téléportation
 
 # Lorsque le joueur entre dans la zone, on l e téléporte
 func _on_body_entered(body: Node3D) -> void:
 	if body.name == "Player":  # Vérifie si c'est le joueur
-		body.global_transform.origin = teleport_position  # Téléporte le joueur à la position définie
+		body.global_transform.origin = teleport_position.global_position  # Téléporte le joueur à la position définie
 		print("🏠 Téléportation vers :", teleport_position)
